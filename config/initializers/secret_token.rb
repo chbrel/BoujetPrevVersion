3f562e64c83a08b5143517cac7c52b46c30a44c6
Christian Brel <ch.brel@gmail.com>
2012-05-20 19:21:59 +0200

    Init commit for Boujet project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boujet01::Application.config.secret_token = '20f588704e9040c47be2781b2ac75e63098fa0d2f9a67a8143446efbc3fb607cb72ec118c085a25bf80309f6fb8e080d4c1a23f8ce52eec36311c01cf8bdb739'
