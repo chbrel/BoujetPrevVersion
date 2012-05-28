9d7e146e63643c66f517f2ae33b989b40b1fb733
Christian Brel <ch.brel@gmail.com>
2012-05-28 02:47:56 +0200

    ignore public/assets and set up assets compilation during deployment

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Boujet01
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.precompile += %w(bootstrap.css)
+    
   end
 end
